0ef4a419184b3e08a53d78341ee801f081cb9b2e
loeken <loeken@internetz.me>
2015-09-12 19:48:50 +0200

    added sort data

--- sort_data.sh
+++ sort_data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm $1_ips.txt $1_ips_unique.txt
-cat $1 | cut -d \  -f 3 | cut -d "." -f 1,2,3,4 > $1_ips.txt
+cat $1.txt | cut -d \  -f 3 | cut -d "." -f 1,2,3,4 > $1_ips.txt
 sort -nu $1_ips.txt > $1_ips_unique.txt
