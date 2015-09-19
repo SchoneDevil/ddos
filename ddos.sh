1906e41fef231f57a43fa242d26572ffb7164175
loeken <loeken@internetz.me>
2015-09-20 00:51:22 +0200

    updated ddos script, sorting ips

--- ddos.sh
+++ ddos.sh
@@ -25,14 +25,41 @@ while /bin/true; do
 		netstat -ant | awk '{print $6}' | sort | uniq -c | sort -n > $filename
 		sed -i 's/)//g' $filename
 		echo "#################TCPDUMP##################"
-		tcpdump -n -s0 -c 1000 >> $filename
+		tcpdump -nn -s0 -c 1000 >> $filename
 		echo "`date` Packets dumped, sleeping now."
 		sleep 1
 		data=`cat $filename`
+		rm -rf /tmp/src_*
+		while read p; do
+		  ip_src=`echo $p | cut -d " " -f 3 | cut -d "." -f 1,2,3,4`
+		  port_src=`echo $p | cut -d " " -f 3 | cut -d "." -f 5`
+		  ip_dst=`echo $p | cut -d " " -f 5 | cut -d "." -f 1,2,3,4`
+		  port_dst=`echo $p | cut -d " " -f 5 | cut -d "." -f 5 | sed 's/\://g'`
+		  if [ "$ip_src" == "$2" ];
+		  then
+		    echo $ip_src >> /tmp/src_ips_us
+		    echo $port_src >> /tmp/src_ports_us
+		  else
+		    echo $ip_src >> /tmp/src_ips_them
+		    echo $port_src >> /tmp/src_ports_them
+		  fi
+		done <$filename
+		filename1="/tmp/temp_data"
+		rm -rf $filename1
+		sort -nu /tmp/src_ips_them > /tmp/src_ips_them_sorted
+		count=`wc -l /tmp/src_ips_them_sorted | awk '{print $1}'`
+		echo $count ips used in attack: >> $filename1
+		cat /tmp/src_ips_them_sorted >> $filename1
+		sort -nu /tmp/src_ports_us > /tmp/src_ports_us_sorted
+		count2=`wc -l /tmp/src_ports_us_sorted | awk '{print $1}'`
+		echo $count2 ports attacked  >> $filename1
+		cat /tmp/src_ports_us_sorted >> $filename1
+
 sendmail -F $sender -it <<END_MESSAGE
 To: $email
 Subject: $subject
 Attack Detected
+`cat $filename1`
 `cat $filename`
 END_MESSAGE
 echo "sendmail complete"
--- sort_data.sh
+++ sort_data.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
-rm $1_ips.txt $1_ips_unique.txt
-cat $1.txt | cut -d \  -f 3 | cut -d "." -f 1,2,3,4 > $1_ips.txt
-sort -nu $1_ips.txt > $1_ips_unique.txt
+#
+# usage ./sort_data.sh <dumpfile> <ip under attack>
+#
+rm -rf src_*
+while read p; do
+  ip_src=`echo $p | cut -d " " -f 3 | cut -d "." -f 1,2,3,4`
+  port_src=`echo $p | cut -d " " -f 3 | cut -d "." -f 5`
+  ip_dst=`echo $p | cut -d " " -f 5 | cut -d "." -f 1,2,3,4`
+  port_dst=`echo $p | cut -d " " -f 5 | cut -d "." -f 5 | sed 's/\://g'`
+  if [ "$ip_src" == "$2" ];
+  then
+    echo $ip_src >> src_ips_us
+    echo $port_src >> src_ports_us
+  else
+    echo $ip_src >> src_ips_them
+    echo $port_src >> src_ports_them
+  fi
+done <$1
+sort -nu src_ips_them > src_ips_them_sorted
+count=`wc -l src_ips_them_sorted | awk '{print $1}'`
+echo $count ips used in attack:
+cat src_ips_them_sorted
+sort -nu src_ports_us > src_ports_us_sorted
+count2=`wc -l src_ports_us_sorted | awk '{print $1}'`
+echo $count2 ports attacked
+cat src_ports_us_sorted
